a40b1a53c670578635050a499364abbaa5d4e1ea
rgraf <roman.graf@ait.ac.at>
2016-09-23 14:52:45 +0200

    #10 Extracted Wikipedie IDs and Descriptions for selected Freebase IDs. Extended classical composition CSV.
    Change-Id: I98e5939ea4f0d7fca7dba13a97f7f28befe0e1df

--- music-genre/src/test/java/eu/europeana/sounds/vocabulary/genres/music/SearchFreebaseForGenres.java
+++ music-genre/src/test/java/eu/europeana/sounds/vocabulary/genres/music/SearchFreebaseForGenres.java
@@ -2,10 +2,14 @@ package eu.europeana.sounds.vocabulary.genres.music;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -29,6 +33,7 @@ public class SearchFreebaseForGenres extends BaseSkosTest {
 	public final String DUMP_FILE = "E:/freebase-rdf-latest/freebase-rdf-latest";
 	public final String ONLY_COMPOSITIONS_CSV_FILE_PATH = "/only_classical_composition_types.csv";
 	public final String ENRICHED_ONLY_COMPOSITIONS_CSV_FILE_PATH = "/enriched_only_classical_composition_types.csv";
+	public final String FREEBASE_DUMP_OUTPUT_FILE_PATH = "/freebaseDumpOutput.txt";
 	
 	private String missingGenres = "Albazo,Aliwen,Ambient jazz,Arabesk,Ayarachi,Ayataki,Bailecito,Balada,"
 			+ "Ballads,Ballet,Beguine,Boli,Bomba,Bomba del Chota,Cachua,Carnaval,Carnavalito,Cha-cha-chá,Chakri,"
@@ -221,7 +226,7 @@ public class SearchFreebaseForGenres extends BaseSkosTest {
 	 * enriched by Description, Freebase ID and preferred labels in different languages. 
 	 * @throws IOException
 	 */
-	@Test
+//	@Test
 	public void retrieveDescriptionsFromDump() throws IOException {
 				
     	List<Concept> concepts = getSkosUtils().retrieveCompositionConceptsFromCsv(
@@ -255,5 +260,94 @@ public class SearchFreebaseForGenres extends BaseSkosTest {
 		assertTrue(res);
 	}
 	
+	
+//	@Test
+	public void extractDBPediaIdFromFreebaseOutput() throws IOException {
+	
+    	List<Concept> concepts = getSkosUtils().retrieveCompositionConceptsFromCsv(
+    			searchAnalysisFodler + ONLY_COMPOSITIONS_CSV_FILE_PATH);    	
+
+    	List<String> freebaseIdList = new ArrayList<String>();
+    	Iterator<Concept> itrConcepts = concepts.iterator();
+    	while (itrConcepts.hasNext()) {
+			Concept concept = itrConcepts.next();
+			String freebaseId = concept.getUri();
+			System.out.println("freebaseId from CSV file: " + freebaseId);
+			String queryFreebaseId = getSkosUtils().freebaseIdToFileName(freebaseId);
+			freebaseIdList.add(queryFreebaseId);
+		}
+
+    	int DESCRIPTION_POS = 2;
+    	String splitBy = "\t";	    
+	    BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(searchAnalysisFodler + FREEBASE_DUMP_OUTPUT_FILE_PATH));
+			String line = "";
+			while ((line = br.readLine()) !=null) {
+				for (String query : freebaseIdList) {
+					if (line.contains("/" + query + ">") 
+							&& line.contains("<http://rdf.freebase.com/key/wikipedia.en_id>")) { 
+					    String[] b = line.split(splitBy);
+						System.out.println("query: " + query + ", wikipedia ID: " + b[DESCRIPTION_POS] + ", line: " + line);
+					}
+				}
+			}
+		    br.close();
+		} catch (FileNotFoundException e1) {
+			System.out.println("File not found. " + e1.getMessage());
+			e1.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IO error. " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	@Test
+	public void extractDBPediaDescriptionsFromFreebaseOutput() throws IOException {
+	
+    	List<Concept> concepts = getSkosUtils().retrieveCompositionConceptsFromCsv(
+    			searchAnalysisFodler + ONLY_COMPOSITIONS_CSV_FILE_PATH);    	
+
+    	List<String> freebaseIdList = new ArrayList<String>();
+    	Iterator<Concept> itrConcepts = concepts.iterator();
+    	while (itrConcepts.hasNext()) {
+			Concept concept = itrConcepts.next();
+			String freebaseId = concept.getUri();
+			System.out.println("freebaseId from CSV file: " + freebaseId);
+			String queryFreebaseId = getSkosUtils().freebaseIdToFileName(freebaseId);
+			freebaseIdList.add(queryFreebaseId);
+		}
+
+		PrintWriter out = new PrintWriter("descriptions_en.txt");
+
+    	int DESCRIPTION_POS = 2;
+    	String splitBy = "\t";	    
+	    BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(searchAnalysisFodler + FREEBASE_DUMP_OUTPUT_FILE_PATH));
+			String line = "";
+			while ((line = br.readLine()) !=null) {
+				for (String query : freebaseIdList) {
+					if (line.contains("/" + query + ">") 
+							&& line.contains("<http://rdf.freebase.com/ns/common.topic.description>") 
+							&& line.contains("@en")) { 
+					    String[] b = line.split(splitBy);
+						out.println("query: " + query + ", wikipedia description: " + b[DESCRIPTION_POS]);
+					}
+				}
+			}
+		    br.close();
+		    out.close();
+		} catch (FileNotFoundException e1) {
+			System.out.println("File not found. " + e1.getMessage());
+			e1.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IO error. " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
 	
 }
